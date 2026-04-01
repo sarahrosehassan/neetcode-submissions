a6b5eb6d6432f7d372caf57d9b4611942ec730d3
Sarah Rose Hassan <59797227+sarahrosehassan@users.noreply.github.com>
2026-03-31 21:16:56 -0400

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+            
+        hashmaps = {}
+        hashmapt = {}
+        # .get(key, default) -- returns value for key, or default if key not found
+        for letter in s:
+            hashmaps[letter] = hashmaps.get(letter, 0) +  1 # need to initialize hashmap with 0 in order to increment
+
+        for letter in t:
+            hashmapt[letter] = hashmapt.get (letter, 0) + 1
+
+        return hashmaps == hashmapt
+
+        # Time: O(n+m) Space: O(1)
