6d531e6c2e41d93b3ff99b70d06dcf16e5b3d6b5
Sarah Rose Hassan <59797227+sarahrosehassan@users.noreply.github.com>
2026-04-01 21:42:31 -0400

    Add: validate-parentheses - submission-4

---
+++ Data Structures & Algorithms/validate-parentheses/submission-4.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        mapping = {')':'(', '}':'{', ']':'['}
+        # Rule: most recently opened bracket must close first (LIFO)
+        # Closing bracket is the key, openign. bracket is the value
+        # only look up mapping when we see a closer
+        # I see a closing bracket, I look up its matching opener
+
+        for bracket in s:
+            if bracket in mapping: # pop if we find closing
+                top = stack.pop() if stack else '#' # remove and return the top bracket
+                if mapping[bracket] != top: # check if top is the expected opening bracket
+                    return False
+            else: # usign else instead of elif bracket in mapping.values() -- values() is O(n), else is O(1)
+                stack.append(bracket)
+
+        #return true if stack is empty else false
+        return not stack
+
+        # time: O(n) space:O(1)
