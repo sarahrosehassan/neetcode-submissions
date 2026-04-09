13c053cb5a6392e25a289849f1018f4a6cf42150
Sarah Rose Hassan <59797227+sarahrosehassan@users.noreply.github.com>
2026-04-08 20:59:08 -0400

    Add: valid-binary-search-tree - submission-5

---
+++ Data Structures & Algorithms/valid-binary-search-tree/submission-5.py
@@ -0,0 +1,35 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        # brute force: go to every and check if left, less than parent(and subtrees) check if right, greater than parent(and subtrees)
+        # O(n^2)
+        # Optimze way: we remember the range each number has to be in
+
+        # helper function dfs checks if the node is valid None return True
+        # if the node is not in the valid range we return False
+        # if we go left, update the max to the current node, if we go right we update the min to the current node
+
+        # initiliaze isvalidbst with root, and range to -infinity and +infinity
+
+        def dfs(node, min_val, max_val):
+            if node is None:
+                return True
+
+            if not (min_val < node.val < max_val):
+                return False
+                
+            return dfs(node.left, min_val, node.val) and dfs(node.right, node.val, max_val)
+
+        
+        
+        return dfs(root, float('-inf'), float('inf'))
+
+
+
+        
