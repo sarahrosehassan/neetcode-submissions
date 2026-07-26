b044a38a6f06c93a8a02a6fc9108613065687e11
Sarah Rose Hassan <59797227+sarahrosehassan@users.noreply.github.com>
2026-07-25 21:39:12 -0400

    Add: majority-element - submission-2

---
+++ Data Structures & Algorithms/majority-element/submission-2.py
@@ -0,0 +1,17 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        # Optimal solution Time: O(n) Space: O(1)
+
+        candidate = None
+        count = 0
+
+        for num in nums:
+            if count == 0:
+                candidate = num
+                count = 1
+            elif num == candidate:
+                count += 1
+            else:
+                count -= 1
+
+        return candidate
